3862e63bc57f0177e8ed6f1c71ef03f49441ec48
Buzin Dmitriy <89111703879@mail.ru>
2025-01-15 21:53:39 +0300

    update project

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from . import views
-from catalog.apps import CatalogConfig
 
-app_name = CatalogConfig.name
+
 app_name = 'catalog'
 urlpatterns = [
     path('home/', views.home_view, name='home'),
     path('contacts/', views.contacts_view, name='contacts'),
-    ]
+    path('product_detail/<int:pk>', views.product_detail, name='product_detail'),
+    path('add_product', views.add_product, name='add_product')
+]
--- catalog/views.py
+++ catalog/views.py
@@ -1,8 +1,36 @@
-from django.shortcuts import render
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+
+from catalog.models import Product, Category
+
 
 def home_view(request):
-    return render(request, 'home.html')
+    products = Product.objects.all()
+    p = Paginator(products, 3)
+    page = request.GET.get('page')
+    try:
+        page_obj = p.get_page(page)  # returns the desired page object
+    except PageNotAnInteger:
+        # if page_number is not an integer then assign the first page
+        page_obj = p.page(1)
+    except EmptyPage:
+        # if page is empty then return last page
+        page_obj = p.page(p.num_pages)
+    context = {'page_obj': page_obj}
+    return render(request, 'products_list.html', context)
+
+
+# def products_list(request):
+#     # Получение всех продуктов из БД
+#     products = Product.objects.all()
+#     return render(request, 'products_list.html', {'products': products})
+
+
+def product_detail(request, pk):
+    # Получение продукта из БД по его ID
+    product = get_object_or_404(Product, pk=pk)
+    return render(request, 'product_detail.html', {'product': product})
 
 
 def contacts_view(request):
@@ -20,3 +48,27 @@ def contact(request):
         # Здесь мы просто возвращаем простой ответ
         return HttpResponse(f"Спасибо, {name}! Ваше сообщение получено.")
     return render(request, 'contact.html')
+
+
+def add_product(request):
+    if request.method == 'POST':
+        # Получение данных из формы
+        name = request.POST.get('name')
+        description = request.POST.get('description')
+        price = request.POST.get('price')
+        category_id = request.POST.get('category')
+        image = request.FILES.get('image')  # Загрузка изображения продукта (если оно есть)
+        stock = request.POST.get('stock')
+        # Создание нового продукта
+        product = Product.objects.create(
+            name=name,
+            description=description,
+            price=price,
+            category_id=category_id,
+            image=image,
+            stock=stock,
+        )
+        return HttpResponse(f"Продукт '{product.name}' успешно добавлен.")
+    # Формирование формы добавления продукта
+    categories = Category.objects.all()
+    return render(request, 'add_product.html', {'categories': categories})
