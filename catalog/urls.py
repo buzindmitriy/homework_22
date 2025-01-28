5580044a30ebb348b0ae5c5e18803dfd3c99d7b6
Buzin Dmitriy <89111703879@mail.ru>
2025-01-29 00:57:21 +0300

    fix urls

--- catalog/urls.py
+++ catalog/urls.py
@@ -6,8 +6,8 @@ app_name = 'catalog'
 urlpatterns = [
     path('home/', views.ProductListView.as_view(), name='home'),
     path('contacts/', views.ContactTemplateView.as_view(), name='contacts'),
-    path('product_detail/<int:pk>', views.ProductDetailView.as_view(), name='product_detail'),
+    path('product_detail/<int:pk>/', views.ProductDetailView.as_view(), name='product_detail'),
     path('add_product', views.ProductCreateView.as_view(), name='add_product'),
-    path('edit_product/<int:pk>', views.ProductUpdateView.as_view(), name='edit_product'),
-    path('delete_product/<int:pk>', views.ProductDeleteView.as_view(), name='delete_product')
+    path('edit_product/<int:pk>/', views.ProductUpdateView.as_view(), name='edit_product'),
+    path('delete_product/<int:pk>/', views.ProductDeleteView.as_view(), name='delete_product')
 ]
